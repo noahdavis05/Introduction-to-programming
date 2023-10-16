ebbfdd3ec238f99999ca569e6722b28fed705471
noahdavis05 <146333718+noahdavis05@users.noreply.github.com>
2023-10-16 21:26:05 +0000

    Homework

---
+++ week3/worksheet7.py
@@ -0,0 +1,73 @@
+def task1():
+    num1 = int(input("Enter an integer number please: "))
+    num2 = int(input("Enter a bigger integer number please: "))
+    count = 0
+    for i in range(num1,num2):
+        count += 1
+
+    return count
+
+def task2():
+    my_list = []
+    for i in range(0,5):
+        temp_list = []
+        for x in range(0,5):
+            temp_list.append(i*5 + x)
+
+        my_list.append(temp_list)
+
+    return my_list
+            
+def task3():
+    num_list = []
+    for i in range(0,10):
+        temp = int(input("Enter a number please:  "))
+        num_list.append(temp)
+
+    print(sum(num_list))
+    print(sum(num_list)/10)
+
+def calc_neighbours(i,x):
+    neighbours = [(i,x-1),(i,x+1),(i-1,x),(i+1,x)]
+    if x == 0:
+        neighbours.remove((i,x-1))
+    if x == 4:
+        neighbours.remove((i,x+1))
+    if i == 0:
+        neighbours.remove((i-1,x))
+    if i == 4:
+        neighbours.remove((i+1,x))
+
+    return neighbours
+
+
+
+
+def task4():
+    num_list = [[1, 4, 0, 1, 3],
+                [2, 2, 4, 2, 2],
+                [1, 0, 1, 0, 1],
+                [0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0]]
+    
+    num_list2 = [[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+    for i in range(0,5):
+        temp = []
+        for x in range(0,5):
+            #check if number 
+            temp_num = num_list[i][x]
+            neighbours = calc_neighbours(i,x) #returns a list of all the locations of the neighbours of the position
+            for location in neighbours:
+                n = num_list[location[0]][location[1]]
+                temp_num += n
+
+            num_list2[i][x] = round(temp_num / len(neighbours))
+
+    return num_list2
+                
+
+
+
+
+
+print(task4())
